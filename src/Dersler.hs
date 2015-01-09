f2477132c8207f8ca1fcf19a127615e1449b530f
Tahsin Gökalp Şaan <info@tahsingokalp.com>
2015-01-09 21:47:51 +0200

    ders2

--- src/Dersler.hs
+++ src/Dersler.hs
@@ -9,7 +9,13 @@ tryhaskell = do
 
 yardim = do
 	putDoc $ green (text "ders1 ") <> yellow (text "=> ") <>  white (text "Basit matematiksel ifadeler.") <> linebreak
+	putDoc $ green (text "ders2 ") <> yellow (text "=> ") <>  white (text "String ifadeler.") <> linebreak
 
 ders1 = do
 	putDoc $ white (text "Haskell'de matematiksel ifadeleri kullanarak hesaplamalar yapabilirsin.İlk olarak bunları deneyebilirsin : ")
-		<> yellow (text "5+7 ") <>  white (text "veya") <> yellow (text "8*10 ") <> linebreak
+		<> yellow (text "5+7 ") <>  white (text "veya ") <> yellow (text "8*10 ") <> linebreak
+
+ders2 = do
+	putDoc $ white (text "Matematiksel ifadelerde sorun yok peki ismimi yazmak istersem?")
+		<> white (text "String ifadeleri Haskell'de tırnak işaretleri arasına yazmanız gerekir.(\" \")")
+		<> yellow (text "\"tahsin\"") <>  white (text " şeklinde sen de kendi ismini dene.") linebreak
